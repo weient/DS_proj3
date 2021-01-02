8dcc2f35249d6150995dfb553d8ef22ca1573dc8
weient <stephanie20001118@gmail.com>
2021-01-03 01:57:09 +0800

    ver2

--- algorithm_ST.cpp
+++ algorithm_ST.cpp
@@ -30,21 +30,40 @@ void algorithm_A(Board board, Player player, int index[]){
 
     //////your algorithm design///////////
     int row = -1, col = -1;
-    int color = player.get_cell_color();
+    int color = player.get_color();
     int highest_score = -20000;
-    int tmp = 0;
     
+    int dir_x[8] = {0, 0, -1, 1, 1, -1, -1, 1};
+    int dir_y[8] = {1, -1, 0, 0, 1, -1, 1, -1};
     
     for(int i=0; i<5; i++)
         for(int j=0; j<6; j++){
+            int tmp = 0;
             if(board.get_cell_color(i, j) == color || board.get_cell_color(i, j) == 'w'){
+                int flag_dangerous = 1;
+                tmp += board.get_orbs_num(i, j);
                 
-                if(board.get_capacity(i, j) == 3)
-                    tmp += 10;
-                if(board.get_capacity(i, j) == 5)
-                    tmp += 5;
-                if(board.get_orbs_num(i, j) == (board.get_capacity(i, j)-1))
-                    tmp += 5;
+                for(int k=0; k<8; k++){
+                    int new_i = i + dir_x[k];
+                    int new_j = j + dir_y[k];
+                    if(new_i >= 0 && new_i <= 4 && new_j >= 0 && new_j <= 5){
+                        if(board.get_cell_color(new_i, new_j) != color && board.get_orbs_num(new_i, new_j) == (board.get_capacity(new_i, new_j) - 1)){
+                            tmp -= (5-board.get_capacity(i, j));
+                            flag_dangerous = 0;
+                        }
+                        if(board.get_cell_color(new_i, new_j) == color){
+                            tmp += 3;
+                        }
+                    }
+                }
+                if(flag_dangerous){
+                    if(board.get_capacity(i, j) == 3)
+                        tmp += 3;
+                    if(board.get_capacity(i, j) == 5)
+                        tmp += 2;
+                    if(board.get_orbs_num(i, j) == (board.get_capacity(i, j)-1))
+                        tmp += 2;
+                }
                 if(tmp > highest_score){
                     highest_score = tmp;
                     row = i;
